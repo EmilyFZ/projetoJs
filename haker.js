3af37536c496939a4350b597716136362431abb0
EmilyFZ <61170696+EmilyFZ@users.noreply.github.com>
2021-04-14 11:36:44 -0300

    Add files via upload

---
+++ haker.js
@@ -0,0 +1,43 @@
+function hacker() {
+    alert("Antivírus Ativado \n Vírus Detectado ")
+
+    document.write(` 
+    <!DOCTYPE html>
+    <html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title> Jogo matrix </title>
+        <link rel="stylesheet" href="matrix.css">
+        <script src="estagiario.js"></script>
+    <style>
+    h1 {
+        text-align: center;
+          padding: 20px;
+            background-color: black;
+            color: white;
+        }
+        button:hover{
+            background-color: rgb(236, 252, 11);
+            color: rgb(240, 15, 15);
+            transition: 3s;
+            font-size: 40px;
+        }
+        </style>
+   
+    </head>
+    
+    <body>
+    <button onclick="escolhido()"> GAMER OVER !</button>
+    
+        </container>
+    
+        <img id="foto1" src="matrix_1.jpg">
+    
+        <div class="footer">
+            Todos os direitos reservados!!
+        </div>
+        </body>
+
+        </html>`)
+}
